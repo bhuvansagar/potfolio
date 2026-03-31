7bebd7a28187467a44911f56f51d66bfd7d0f8f6
Bhuwan Sagar <bhuvansagar16022004@gmail.com>
2026-03-31 13:24:07 +0530

    index.html

---
+++ script.js
@@ -0,0 +1,27 @@
+/*== scroll==*/
+
+let sections = document.querySelectorAll('section');
+let navlink = document.querySelectorAll('header nav a');
+
+window.onscroll=() => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset =sec.offsetTop -150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height ) {
+           navlink.forEach(link => {
+            links.classList.remove('active');
+              document.querySelector('header nav a[href*=' + id +']').classList.add('active');
+           });
+        };
+    });
+
+    /* sticky navbar */
+
+    let header = document.querySelector();
+    header.classList.toggle('stricky', window .scrollY > 100);
+    navlink.forEach(link => link.classList.remove('active') );
+    
+};
